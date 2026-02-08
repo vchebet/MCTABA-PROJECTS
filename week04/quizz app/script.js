2a2522ca75fe9e6512af59e20f79842367fd9bcc
vchebet <valarichebet@gmail.com>
2026-02-08 21:37:59 +0300

    initial commit

---
+++ week04/quizz app/script.js
@@ -0,0 +1,30 @@
+const quizes = [
+  {
+    q: "What is this?",
+    choices:["a", "B", "c"],
+    answer: "a"
+  },
+
+
+  { 
+    q: "What are those?",
+    choices:["a", "B", "c"],
+    answer: "a"
+  },
+
+
+  {
+    q: "What is that?",
+    choices:["a", "B", "c"],
+    answer: "a"
+  }
+]
+const quizbox =document.getElementById("quizbox");
+const quizText =document.getElementById("quizbox");
+const answers =document.getElementById("quizbox");
+const scoreText = document.getElementById("quizbox");
+const nextBtn =document.getElementById("quizbox");
+const restartBtn=document.getElementById("quizbox");
+const feedback=document.getElementById("quizbox");
+
+
